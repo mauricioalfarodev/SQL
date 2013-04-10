b2ab738399b7d08caa7644b565a513c43b611e86
Mauricio alfaro <mauricio-alfaro@hotmail.com>
2013-04-10 14:59:34 -0600

    app

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function main() {
+		document.addEventListener("deviceready", aplicacionIniciada, false); // Al inciar la app
+		document.addEventListener("pause", aplicaciónPausada, false);        // Al pausar la app
+		document.addEventListener("resume", aplicaciónReiniciada, false);    // Al reiniciar la app
+		document.addEventListener("online", phonegapOnline, false);          // Phonegap tiene acceso a internet
+		document.addEventListener("offline", phonegapOffline, false);        // Phonegap NO tiene acceso a internet
+		document.addEventListener("backbutton", atrasPulsado, false);        // Se ha pulsado la tecla atrás
+		document.addEventListener("menubutton", menuPulsado, false);         // Se ha pulsado la tecla menú
+		document.addEventListener("searchbutton", menuPulsado, false);       // Se ha pulsado la tecla búsqued
+}
+
+function aplicacionIniciada()
+{
+	CreaDB();
+}
+ 
+function aplicaciónPausada()
+{
+}
+ 
+function aplicaciónReiniciada()
+{
+}
+ 
+function phonegapOnline()
+{
+}
+ 
+function phonegapOffline()
+{
+}
+ 
+function atrasPulsado()
+{
+}
+ 
+function menuPulsado()
+{
+}
+ 
+function busquedaPulsado()
+{
+}
